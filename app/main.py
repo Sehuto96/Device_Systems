379ddbc1de593c57c8df2bbc2c439c1eb6a4bf67
Sebastian Hurtado <sehuto96@gmail.com>
2026-08-27 23:32:44 -0500

    Fase 5: middleware con cabeceras personalizadas X-App-Name y X-API-Version

--- app/main.py
+++ app/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 from app.routes import user_routes
 
 app = FastAPI(title="device_systems", version="1.0.0")
+
+
+@app.middleware("http")
+async def add_custom_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["X-App-Name"] = "device_systems"
+    response.headers["X-API-Version"] = "1.0"
+    return response
+
+
 app.include_router(user_routes.router)
 
 
